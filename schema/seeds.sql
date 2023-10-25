81b85a5db7ec7437058ac13086d63e4891c3bd36
Kevin Patto <kevp2010@gmail.com>
2023-10-24 20:44:51 -0500

    fixing errors

--- schema/seeds.sql
+++ schema/seeds.sql
@@ -1,20 +1,20 @@
 INSERT INTO department (name)
 VALUES ('R&D'),
-       ('Useless People'),
-       ('The Forbidden Ones'),
-       ('People named Jeffrey')
+       ('Gross Things'),
+       ('[REDACTED]'),
+       ('Jeffries');
 
 INSERT INTO role (title, salary, department_id)
 VALUES ('Researcher', 25000, 1),
        ('Developer', 28999, 1),
        ('Ampersand', 450000, 1),
-       ('Leeches', 2400, 2),
-       ('Slugs', 2690, 2),
-       ('Cthontic Gods', 600000, 3),
-       ('Ascended Beings', 400000, 3),
+       ('Leech', 2400, 2),
+       ('Slug', 2690, 2),
+       ('Cthontic God', 600000, 3),
+       ('Ascended One', 400000, 3),
        ('Cryptids', 2500, 3),
        ('Finders', 40000, 4),
-       ('Makers', 45600, 4)
+       ('Makers', 45600, 4);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ('Bob', 'Bobert', 1, 3),
@@ -28,4 +28,4 @@ VALUES ('Bob', 'Bobert', 1, 3),
        ('Big', 'Foot', 8, 8),
        ('The', 'Bibendum', 8, NULL),
        ('Jeffery', 'Finder', 9, NULL),
-       ('Jeffery', 'Maker', 10, 11)
+       ('Jeffery', 'Maker', 10, 11);
