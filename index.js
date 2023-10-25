944c0969d20f1f344dd5b93378f758d4c9e9fbab
Kevin Patto <kevp2010@gmail.com>
2023-10-24 20:46:24 -0500

    woops i accidentally sent my sql password xd

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const mysql = require('mysql2');
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user: 'root',
-        password: 'rooters',
-        database: 'employees_db'
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     }
 );
 
